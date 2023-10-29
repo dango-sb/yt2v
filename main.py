4611dcb4df4f6abbd939824d8143175371ec695d
mxm v <disciplina.dot8@gmail.com>
2023-10-29 10:46:01 +0200

    Error for existing files

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import os
 
 # create savepath file
 # TODO Error if path is incorrect in savepath.txt
-save_path = 'D:\Music'
 savepath = open("savepath.txt", 'a+')
 savepath_video=savepath.readline()
 savepath_audio=savepath.readline()
@@ -30,6 +29,13 @@ root.resizable(width=False, height=False)
 custom_font = ('Arial', 16)
 
 # Function to download audio
+def existsErr(file_path):
+    if os.path.exists(file_path):
+        log.insert("end", "File with this name already exists.\n")
+        return True
+    else:
+        return False
+    
 def downloadAudio():
     url = entry.get()
     if ("https://youtu.be/" != url[:17] and "https://www.youtube.com/" != url[:24]):
@@ -37,11 +43,16 @@ def downloadAudio():
     else:
         yt = YouTube(url)
         audio_stream = yt.streams.filter(only_audio=True, file_extension="mp4").first()
-        audio_stream.download(output_path=save_path)
         original_filename = audio_stream.default_filename
-        new_filename = original_filename.replace(".mp4", ".mp3")
+        
         import os
-        os.rename(os.path.join(save_path, original_filename), os.path.join(save_path, new_filename))
+        if existsErr(os.path.join(savepath_audio, original_filename)):
+            return
+        
+        audio_stream.download(output_path=savepath_audio)
+        new_filename = original_filename.replace(".mp4", ".mp3")
+        
+        os.rename(os.path.join(savepath_audio, original_filename), os.path.join(savepath_audio, new_filename))
         log.insert("end", f'Download of {new_filename} is finished successfully.\n')
     entry.delete(0, "end")
 
@@ -53,9 +64,14 @@ def downloadVideo():
     else:
         yt = YouTube(url)
         audio_stream = yt.streams.filter(only_audio=True, file_extension="mp4").first()
-        audio_stream.download(output_path=save_path)
+        original_filename = audio_stream.default_filename
+        
+        import os
+        if existsErr(os.path.join(savepath_audio, original_filename)):
+            return
+        
+        audio_stream.download(output_path=savepath_video)
         log.insert("end", f'Download of {audio_stream.default_filename} is finished successfully.\n')
-
     entry.delete(0, "end")
 
 # Function to switch to audio download
@@ -72,7 +88,7 @@ def changeToVideo():
 frame = tk.Frame(root, padx=3, pady=3)  # Create a frame with padx and pady
 entry = tk.Entry(frame)
 downloadButton = ttk.Button(frame, text="Download", command=downloadAudio)
-log = tk.Text(root, width=40, height=10, padx=5, state="disabled")
+log = tk.Text(root, width=40, height=10, padx=5, state="normal")
 formatLabel = ttk.Label(text='Audio', font=custom_font)
 
 # Menu
